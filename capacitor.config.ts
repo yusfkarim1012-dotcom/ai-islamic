af67ca3574b9e5db3df7c00f62672613b0a04a34
Yusf Karim <yusfkarim@example.com>
2026-05-18 19:48:42 +0300

    feat: Configure Android application name to 'پرسیاری شەرعی' and automatically generate native Android launcher icons from PWA sources

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.aikurdi.app',
-  appName: 'aikurdi',
+  appName: 'پرسیاری شەرعی',
   webDir: 'dist',
   plugins: {
     CapacitorHttp: {
